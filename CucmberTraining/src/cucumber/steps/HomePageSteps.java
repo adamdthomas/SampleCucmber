1c1973aef3ee071647770d769f6223146764fdbd
Adam THomas <adamdthomas@gmail.com>
2017-10-18 10:27:04 -0400

    Added homePage

--- CucmberTraining/src/cucumber/steps/HomePageSteps.java
+++ CucmberTraining/src/cucumber/steps/HomePageSteps.java
@@ -10,10 +10,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.After;
 import cucumber.api.java.en.*;
+import lfg.lfgdotcom.HomePage;
 
 public class HomePageSteps {
 	
 	WebDriver driver;
+	HomePage homepage;
 	
 	@Given("^I navigate to the lfg homepage$")
 	public void i_navigate_to_the_lfg_homepage() throws Throwable {
@@ -32,22 +34,21 @@ public class HomePageSteps {
 	
 	@When("^I enter my username \"(.*)\"$")
 	public void i_enter_my_usernameAL(String userName) throws Throwable {
-		driver.findElement(By.id("banner-username")).sendKeys(userName);
-		System.out.println("Entering my username");
+		homepage = new HomePage(driver);
+		homepage.EnterUserName(userName);
 	}
 	
 
 	@When("^I click next$")
 	public void i_click_next() throws Throwable {
-		driver.findElement(By.id("next-btn-banner")).click();
-		System.out.println("Clicking Next");
+		homepage.GoToLoginPage();
 	}
 
 	@Then("^I should be at the sign on page$")
 	public void i_should_be_at_the_sign_on_page() throws Throwable {
-		String title = driver.getTitle();
-		Assert.assertEquals("Sign On", title);
-		System.out.println("At the sign in page");
+
+		Assert.assertEquals("Sign On", homepage.pageTitle);
+
 	}
 
 	
